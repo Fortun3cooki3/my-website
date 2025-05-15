d86cfa53c50c2641d7a5b819b538a8ddb3288eeb
Fortun3cooki3 <hierismerlijn@hotmail.com>
2025-05-15 23:26:23 +0200

    fix for wsgi and whitenoise

--- my_first_blog/wsgi.py
+++ my_first_blog/wsgi.py
@@ -8,10 +8,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_first_blog.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = WhiteNoise(application)
