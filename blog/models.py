7512f18f426f39132c1ed9c185a9a73d4f6930df
Fortun3cooki3 <hierismerlijn@hotmail.com>
2025-05-16 16:40:57 +0200

    extra bucket functions and new upload from image to media

--- blog/models.py
+++ blog/models.py
@@ -17,7 +17,7 @@ class Projects(models.Model):
   full_description = models.TextField(blank=True)
   url = models.URLField(blank=True, default="https://www.linkedin.com/in/merlijn-wolters-0b3450140/")
   git_hub = models.URLField(blank=True, default="https://www.linkedin.com/in/merlijn-wolters-0b3450140/")
-  image = models.ImageField(null=True, blank=True, upload_to='images')
+  image = models.ImageField(null=True, blank=True, upload_to='media/')
   slug = models.SlugField( default= "", null=False, blank=True, db_index=True)
 
   def get_absolute_url(self):
@@ -31,7 +31,7 @@ class Cv (models.Model):
   title = models.CharField(max_length=50)
   description = models.TextField()
   url = models.URLField(null=True, blank=True)
-  image = models.ImageField(null=True, blank=True, upload_to='images/')
+  image = models.ImageField(null=True, blank=True, upload_to='media/')
   skills = models.ManyToManyField(Skills)
   start_date = models.DateField(null=True, blank=True)
   end_date = models.DateField(null=True, blank=True)
@@ -41,7 +41,7 @@ class Cv (models.Model):
   
 class CoverLetter(models.Model):
   my_name = models.CharField(max_length=50)
-  picture = models.ImageField(null=True, blank=True, upload_to='images/')
+  picture = models.ImageField(null=True, blank=True, upload_to='media/')
   coverletter = models.TextField(blank=True)
 
   def __str__(self):
