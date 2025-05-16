ed07aec1f97c7f4ef3d4e67a09ec8dc822106252
Fortun3cooki3 <hierismerlijn@hotmail.com>
2025-05-16 18:12:06 +0200

    changed secrets

--- my_first_blog/settings.py
+++ my_first_blog/settings.py
@@ -165,3 +165,5 @@ DEFAULT_FILE_STORAGE = "storage.supabase_storage.SupabaseStorage"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField" 
+
+#for heroku
