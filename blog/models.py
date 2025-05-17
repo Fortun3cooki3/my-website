02261ece7f7dc3e75ae8d1c37606bdd606d18ea6
Fortun3cooki3 <hierismerlijn@hotmail.com>
2025-05-17 13:13:08 +0200

    bucket-upload fixes

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 from django.urls import reverse
-from storage.supabase_storage import SupabaseStorage
+from supabase_storage import SupabaseStorage
 
 # Create your models here.
 
