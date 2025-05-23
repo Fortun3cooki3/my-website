fde8c0c3bd27e38f029160d578d8ad021e59db7e
Fortun3cooki3 <hierismerlijn@hotmail.com>
2025-05-23 09:55:31 +0200

    fix

--- my_first_blog/settings.py
+++ my_first_blog/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
+ALLOWED_HOSTS = ["mvdmeer-site-f4ab49af64b1.herokuapp.com"]
 
 
-CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", Csv())
+CSRF_TRUSTED_ORIGINS = ["https://mvdmeer-site-f4ab49af64b1.herokuapp.com"]
 # Application definition
 
 INSTALLED_APPS = [
