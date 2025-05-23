b53865818d1eec5287c65b2487bf986a51990c21
Fortun3cooki3 <hierismerlijn@hotmail.com>
2025-05-23 09:48:54 +0200

    quick fix

--- my_first_blog/settings.py
+++ my_first_blog/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = [config("ALLOWED_HOSTS")]
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 
 
-CSRF_TRUSTED_ORIGINS = [config("CSRF_TRUSTED_ORIGINS")]
+CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", Csv())
 # Application definition
 
 INSTALLED_APPS = [
